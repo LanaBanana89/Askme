9157f4b066e632f73a9135caf5f1394f8a39eed1
LanaBanana <s.glazyrina@mail.ru>
2018-04-18 14:20:35 +0300

    Add questions count in show.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,33 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def sklonenie(number, krokodil, krokodila, krokodilov)
+    if number == nil || !number.is_a?(Numeric)
+      number = 0
+    end
+
+    #Рассчитываем остаток от деления на сто
+    ostatok_100 = number % 100
+
+    # Проверяем, входит ли остаток от деления на сто в период 11..14
+    if (11..14).include?(ostatok_100)
+      return krokodilov
+    end
+
+    ostatok = number % 10
+
+    if ostatok == 1
+      return krokodil
+    end
+
+    if ostatok >= 2 && ostatok <= 4
+      return krokodila
+    end
+
+
+    if ostatok >= 5 && ostatok <= 9 || ostatok == 0
+      return krokodilov
+    end
+  end
 end
